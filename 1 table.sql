dbea851644ba9539077cfa06481d8fa83bcc2d67
yisraelkramer <yisraelkramer@gmail.com>
2024-08-09 14:34:26 -0400

    added sample data

--- 1 table.sql
+++ 1 table.sql
@@ -1,5 +1,7 @@
 use humanBodyDb 
 go 
 create table dbo.body(
-    BodyID int not null identity primary key 
+    BodyID int not null identity primary key,
+    BodyPart varchar(100) not null, 
+    Num int not null
 )
